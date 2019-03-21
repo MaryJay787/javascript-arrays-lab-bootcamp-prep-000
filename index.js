1f26aa440bccca6072bb2e765f00f8c0ccfd7f87
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-03-21 23:32:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function destructivelyRemoveLastKitten() {
 function destructivelyRemoveFirstKitten() {
   return kittens.shift();
 }
+
+function appendKitten(name) {
+  kittens.slice(1);
+  return kittens;
+}
